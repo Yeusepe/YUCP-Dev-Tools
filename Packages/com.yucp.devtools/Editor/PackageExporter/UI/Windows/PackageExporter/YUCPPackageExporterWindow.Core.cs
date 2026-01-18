950c56a10a4c3e3d5ae55c3a2df4222dc2ecb197
YUCP Creations <subunit_swings0h@icloud.com>
2026-01-17 22:10:32 -0500

    Remove default export folder in demo package profile
    The demo package profile no longer sets 'Assets' as the default folder to export, resulting in an empty export folder list. Also incremented package version to 0.2.31.

--- Packages/com.yucp.devtools/Editor/PackageExporter/UI/Windows/PackageExporter/YUCPPackageExporterWindow.Core.cs
+++ Packages/com.yucp.devtools/Editor/PackageExporter/UI/Windows/PackageExporter/YUCPPackageExporterWindow.Core.cs
@@ -162,7 +162,7 @@ namespace YUCP.DevTools.Editor.PackageExporter
             profile.description = "This is a demo package to help you get started with the Package Exporter.";
             profile.author = "You";
             
-            profile.foldersToExport = new List<string> { "Assets" };
+            profile.foldersToExport = new List<string>();
             
             AssetDatabase.CreateAsset(profile, demoPath);
             AssetDatabase.SaveAssets();
